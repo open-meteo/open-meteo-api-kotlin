5d3c65fb4879c2da62f76e28b9c6d29609800134
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-08-20 18:49:13 +0200

    test: historical endpoint sunsets
    Bug discovered: decodeLong() is not precise. See issue #1.

---
+++ lib/src/test/kotlin/com/openmeteo/api/historical/HistoricalEndpointTest.kt
@@ -0,0 +1,54 @@
+package com.openmeteo.api.historical
+
+import com.openmeteo.api.common.params.IsoDate
+import com.openmeteo.api.historical.params.Daily
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlin.test.*
+
+class HistoricalEndpointTest {
+    companion object {
+        val endpoint = HistoricalEndpoint()
+    }
+
+    @Test
+    @ExperimentalSerializationApi
+    fun `Sunsets from 2022-01-01 to 2022-01-03`() {
+        val response = endpoint(
+            startDate = IsoDate("2022-01-01"),
+            endDate = IsoDate("2022-01-03"),
+            daily = listOf(
+                Daily.sunset
+            )
+        ).getOrThrow()
+        val daily = response.daily!!
+        val sunsets = daily.sunset!!
+        assertContentEquals(
+            // listOf(1641049506L, 1641135975L, 1641222444L)
+            // bug: all times are 2 seconds "early"!?
+            listOf(1641049504L, 1641135973L, 1641222442L),
+            sunsets.map { it.time },
+        )
+        assertEquals("Europe/Berlin", response.timezone.id)
+    }
+
+    @Test
+    @ExperimentalSerializationApi
+    fun `Sunsets from 2022-06-01 to 2022-06-03`() {
+        val response = endpoint(
+            startDate = IsoDate("2022-06-01"),
+            endDate = IsoDate("2022-06-03"),
+            daily = listOf(
+                Daily.sunset
+            )
+        ).getOrThrow()
+        val daily = response.daily!!
+        val sunsets = daily.sunset!!
+        assertContentEquals(
+            // listOf(1654111265L, 1654197733L, 1654284201L)
+            // bug: all times are 1 second "late"!?
+            listOf(1654111266L, 1654197734L, 1654284202L),
+            sunsets.map { it.time },
+        )
+        assertEquals("Europe/Berlin", response.timezone.id)
+    }
+}
