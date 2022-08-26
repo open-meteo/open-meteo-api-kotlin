729fc09a6f1ef14b219fb317bdd77d948df7b437
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-08-26 15:48:53 +0200

    chore: use tags in update release workflow
    Automatic versioning and version bumping is not implemented.

---
+++ .github/scripts/body.sh
@@ -0,0 +1,50 @@
+declare -A types=(
+  [fix]=":bug: Fixes"
+  [feat]=":rocket: New features"
+  # [refactor]=":building_construction: Refactoring"
+  [build]=":hammer: Build"
+  [test]=":test_tube: Tests"
+  [chore]=":wrench: Chores"
+  [docs]=":orange_book: Docs"
+  # [perf]=":zap: Performance"
+  # [style]=":art: Style"
+)
+
+# [ -d body ] && rm -rf body # useful when debugging
+mkdir body
+
+while read -r commit; do
+    
+  IFS=' ' read -r -a c <<< "$commit"
+
+  url="${c[0]}"
+  
+  [ "${c[1]: -1}" != ':' ] \
+  && echo "Warning: in commit '${url:2:7}': missing <type>!" 1>&2 \
+  && continue
+
+  type="${c[1]:: -1}" # trim ':'
+  [ "${type: -1}" == "!" ] && touch body/!
+  type="${type/%\!/}" # trim '!' at the end
+
+  [ ! -v "types[$type]" ] \
+  && echo "Warning: in commit '${url:2:7}': '$type' is not a mapped type!" 1>&2 \
+  && continue
+
+  title="${c[@]:2}"
+  echo " - ${c[0]} - ${title^}" >> "body/$type"
+
+done < commits
+
+# the order we want is the following:
+for type in feat fix docs test chore build
+do [ -a "body/$type" ] && cat<<EOF
+## ${types[$type]}
+
+$(cat "body/$type")
+
+EOF
+done
+
+# return 0 even if last type file was missing
+exit 0
--- .github/workflows/release/latest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# get latest release tag with pre-releases
-# see: https://stackoverflow.com/a/52680984/9373031
-echo "latest=$(git -c 'versionsort.suffix=-' tag --sort=-v:refname \
-| grep -E -o -m 1 "$regex" | head -1)"
