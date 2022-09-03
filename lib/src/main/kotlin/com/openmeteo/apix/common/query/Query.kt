ce41225214da2e292d7261c7b8ef9d377656ce6e
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-03 22:09:26 +0200

    refactor(apix): query key superclass serial name
    Walk the inheritance list until a serial name annotation is found.

--- lib/src/main/kotlin/com/openmeteo/apix/common/query/Query.kt
+++ lib/src/main/kotlin/com/openmeteo/apix/common/query/Query.kt
@@ -2,18 +2,27 @@ package com.openmeteo.apix.common.query
 
 import kotlinx.serialization.*
 import java.net.URL
+import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.superclasses
 
 interface Query {
 
     companion object {
 
-        internal fun key(property: KProperty1<Query, *>) =
-            property.findAnnotation<SerialName>()
-                ?.value ?: property.name
+        internal fun key(property: KProperty1<Query, *>, kClass: KClass<*>) =
+            property.findAnnotation<SerialName>()?.value
+                ?: kClass.superclasses.firstNotNullOfOrNull { superClass ->
+                    superClass.declaredMemberProperties
+                        .firstOrNull { it.name == property.name }
+                        ?.findAnnotation<SerialName>()?.value
+                } ?: property.name
+
+        internal inline fun <reified T> key(property: KProperty1<Query, *>) =
+            key(property, T::class)
 
         @OptIn(InternalSerializationApi::class, ExperimentalSerializationApi::class)
         internal fun value(any: Any?) : String =
@@ -42,7 +51,7 @@ interface Query {
      */
     fun toList () =
         nonTransientDeclaredMemberProperties
-            .mapNotNull { value(it)?.let { v -> key(it) to v } }
+            .mapNotNull { value(it)?.let { v -> key(it, javaClass.kotlin) to v } }
 
     fun toMap() =
         toList().toMap()
