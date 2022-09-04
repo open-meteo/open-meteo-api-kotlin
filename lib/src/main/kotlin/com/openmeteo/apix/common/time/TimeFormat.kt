7bd3a0a8238f5e9e9b71a3650b2dacda926ca635
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-04 11:19:17 +0200

    feat(apix): time format enum

---
+++ lib/src/main/kotlin/com/openmeteo/apix/common/time/TimeFormat.kt
@@ -0,0 +1,17 @@
+package com.openmeteo.apix.common.time
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * How timestamps should be formatted
+ *
+ * Used internally to hardcode unix [Long] values, as easier/safer to parse
+ */
+@Serializable
+enum class TimeFormat {
+    @SerialName("iso8601")
+    Iso8601,
+    @SerialName("unixtime")
+    UnixTime,
+}
