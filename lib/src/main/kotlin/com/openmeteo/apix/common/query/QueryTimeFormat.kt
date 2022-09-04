2637e388e304b2909882e4c281daefd3ad89961e
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-04 11:19:41 +0200

    feat(apix): time format query

---
+++ lib/src/main/kotlin/com/openmeteo/apix/common/query/QueryTimeFormat.kt
@@ -0,0 +1,9 @@
+package com.openmeteo.apix.common.query
+
+import com.openmeteo.apix.common.time.TimeFormat
+import kotlinx.serialization.SerialName
+
+interface QueryTimeFormat : Query {
+    @SerialName("timeformat")
+    val timeFormat: TimeFormat
+}
