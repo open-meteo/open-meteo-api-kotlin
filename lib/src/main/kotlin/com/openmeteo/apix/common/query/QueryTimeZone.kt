8fda0f21fde6562675d5973d16657d858b5f2dbd
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-04 11:19:32 +0200

    feat(apix): timezone query

--- lib/src/main/kotlin/com/openmeteo/apix/common/query/QueryTimeZone.kt
+++ lib/src/main/kotlin/com/openmeteo/apix/common/query/QueryTimeZone.kt
@@ -1,5 +1,9 @@
 package com.openmeteo.apix.common.query
 
+import com.openmeteo.apix.common.time.TimeZone
+import kotlinx.serialization.SerialName
+
 interface QueryTimeZone : Query {
-    // val timeZone: TimeZone
+    @SerialName("timezone")
+    val timeZone: TimeZone
 }
