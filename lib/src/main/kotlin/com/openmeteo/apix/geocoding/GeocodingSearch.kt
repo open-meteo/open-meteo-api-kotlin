cce417d0b62236d85f2d7fb6fe36dbebb82ace42
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-06 21:28:00 +0200

    feat(apix): geocoding get and search endpoints

---
+++ lib/src/main/kotlin/com/openmeteo/apix/geocoding/GeocodingSearch.kt
@@ -0,0 +1,41 @@
+package com.openmeteo.apix.geocoding
+
+import com.openmeteo.apix.common.http.ContentFormat
+import com.openmeteo.apix.common.http.Endpoint
+import com.openmeteo.apix.common.query.Query
+import com.openmeteo.apix.common.query.QueryContentFormat
+import com.openmeteo.apix.common.response.ResponseGenerationTimed
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.net.URL
+
+open class GeocodingSearch(
+    val name: String,
+    @SerialName("count")
+    val count: Int? = null,
+    val language: String? = null,
+    context: URL = Companion.context,
+) : Endpoint(context),
+    QueryContentFormat {
+
+    override val format: ContentFormat = ContentFormat.ProtoBuf
+
+    companion object {
+        val context = URL("https://geocoding-api.open-meteo.com/v1/search")
+    }
+
+    init {
+        count?.let { require(it in 1..100) }
+    }
+
+    @Serializable
+    open class Response(
+        val results: Array<GeocodingGet.Response>,
+        @SerialName("generationtime_ms")
+        override val generationTimeMs: Float,
+    ) : ResponseGenerationTimed
+
+    operator fun invoke() = invoke<Response>()
+    operator fun invoke(query: Query) = invoke<Response>(query)
+
+}
