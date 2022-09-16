be37a36b7551d1cdede5641ac2207a477ed6b1c4
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-16 12:14:06 +0200

    feat(apix): Ecmwf endpoint

--- lib/src/main/kotlin/com/openmeteo/apix/OpenMeteo.kt
+++ lib/src/main/kotlin/com/openmeteo/apix/OpenMeteo.kt
@@ -7,6 +7,10 @@ import com.openmeteo.apix.common.http.Endpoint
 import com.openmeteo.apix.common.query.QueryCoordinates
 import com.openmeteo.apix.common.time.Date
 import com.openmeteo.apix.common.time.TimeZone
+import com.openmeteo.apix.common.units.PrecipitationUnit
+import com.openmeteo.apix.common.units.TemperatureUnit
+import com.openmeteo.apix.common.units.WindSpeedUnit
+import com.openmeteo.apix.ecmwf.Ecmwf
 import com.openmeteo.apix.elevation.Elevation
 import com.openmeteo.apix.geocoding.GeocodingGet
 import com.openmeteo.apix.geocoding.GeocodingSearch
@@ -15,6 +19,7 @@ class OpenMeteo(
     override val latitude: Float = 0f,
     override val longitude: Float = 0f,
     val airQuality: Endpoint = Endpoint(AirQuality.context),
+    val ecmwf: Endpoint = Endpoint(Ecmwf.context),
     val elevation: Endpoint = Endpoint(Elevation.context),
     val geocodingGet: Endpoint = Endpoint(GeocodingGet.context),
     val geocodingSearch: Endpoint = Endpoint(GeocodingSearch.context),
@@ -36,6 +41,9 @@ class OpenMeteo(
     operator fun invoke(query: AirQuality.Query) =
         airQuality.query<AirQuality.Response>(query)
 
+    operator fun invoke(query: Ecmwf.Query) =
+        ecmwf.query<Ecmwf.Response>(query)
+
     operator fun invoke(query: Elevation.Query) =
         elevation.query<Elevation.Response>(query)
 
@@ -56,6 +64,20 @@ class OpenMeteo(
         pastDays: Int? = null,
     ) = invoke(AirQuality.Query(latitude, longitude, hourly, domains, timeZone,
         startDate, endDate, pastDays))
+
+    fun ecmwf(
+        latitude: Float = this.latitude,
+        longitude: Float = this.longitude,
+        hourly: Iterable<com.openmeteo.apix.ecmwf.Hourly>? = null,
+        temperatureUnit: TemperatureUnit? = null,
+        windSpeedUnit: WindSpeedUnit? = null,
+        precipitationUnit: PrecipitationUnit? = null,
+        timeZone: TimeZone? = null,
+        startDate: Date? = null,
+        endDate: Date? = null,
+        pastDays: Int? = null,
+    ) = invoke(Ecmwf.Query(latitude, longitude, hourly, temperatureUnit,
+        windSpeedUnit, precipitationUnit, timeZone, startDate, endDate, pastDays))
 
     fun elevation(
         vararg coordinates: Pair<Float, Float>,
---
+++ lib/src/main/kotlin/com/openmeteo/apix/ecmwf/Ecmwf.kt
@@ -0,0 +1,56 @@
+package com.openmeteo.apix.ecmwf
+
+import com.openmeteo.apix.common.query.*
+import com.openmeteo.apix.common.response.ResponseCoordinates
+import com.openmeteo.apix.common.response.ResponseGenerationTimed
+import com.openmeteo.apix.common.response.ResponseHourly
+import com.openmeteo.apix.common.time.Date
+import com.openmeteo.apix.common.time.TimeZone
+import com.openmeteo.apix.common.units.PrecipitationUnit
+import com.openmeteo.apix.common.units.TemperatureUnit
+import com.openmeteo.apix.common.units.Unit
+import com.openmeteo.apix.common.units.WindSpeedUnit
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.net.URL
+
+object Ecmwf {
+
+    val context = URL("https://air-quality-api.open-meteo.com/v1/air-quality")
+
+    class Query(
+        override val latitude: Float,
+        override val longitude: Float,
+        override val hourly: Iterable<Hourly>? = null,
+        val temperatureUnit: TemperatureUnit? = null,
+        val windSpeedUnit: WindSpeedUnit? = null,
+        val precipitationUnit: PrecipitationUnit? = null,
+        override val timeZone: TimeZone? = null,
+        override val startDate: Date? = null,
+        override val endDate: Date? = null,
+        override val pastDays: Int? = null,
+    ) : QueryCoordinates,
+        QueryHourly,
+        QueryDateRange
+
+    @Serializable
+    class Response(
+        override val latitude: Float,
+        override val longitude: Float,
+        @SerialName("utc_offset_seconds")
+        override val utcOffsetSeconds: Int,
+        @SerialName("timezone")
+        override val timeZone: TimeZone,
+        @SerialName("timezone_abbreviation")
+        override val timeZoneAbbreviation: String,
+        @SerialName("hourly_units")
+        override val hourlyUnits: Map<Hourly, Unit>? = null,
+        @SerialName("hourly")
+        override val hourlyValues: Map<Hourly, Array<Double?>>? = null,
+        @SerialName("generationtime_ms")
+        override val generationTimeMs: Float,
+    ) : ResponseCoordinates,
+        ResponseHourly<Hourly>,
+        ResponseGenerationTimed
+
+}
