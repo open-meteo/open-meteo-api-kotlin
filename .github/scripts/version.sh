22ec65743ed706b259c572e9dd9adf915e7c8ea7
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-20 18:54:11 +0200

    chore: version.sh bump function
    In the future automatic versioning is going to be implemented.

---
+++ .github/scripts/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+bump() {
+  [ -v "version" ] || exit 1
+
+  sed -i "s/version = '.*'/version = '$version'/" "lib/build.gradle"
+  sed -i "s/implementation 'com.open-meteo:open-meteo-api-kotlin:.*'/implementation 'com.open-meteo:open-meteo-api-kotlin:$version'/" "INSTALLATION.md"
+  sed -i "s/<version>.*<\/version>/<version>$version<\/version>/" "INSTALLATION.md"
+
+  git add "lib/build.gradle" "INSTALLATION.md"
+  git commit -m "Version $version" -- "lib/build.gradle" "INSTALLATION.md"
+  git tag "$version"
+  # git push origin "$version" --tags
+}
+
+if [ "$1" == "bump" ]; then
+  version="$2" bump
+fi
