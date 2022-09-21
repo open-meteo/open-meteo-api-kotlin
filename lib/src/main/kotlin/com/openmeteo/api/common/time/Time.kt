49c927e91bd0e7f48d7da658bcbe65a7bfcd80ef
DadiBit <38182988+DadiBit@users.noreply.github.com>
2022-09-21 14:50:20 +0200

    feat: `Time` constructor for `Double`s
    Ease `Array<Double>` conversion.

--- lib/src/main/kotlin/com/openmeteo/api/common/time/Time.kt
+++ lib/src/main/kotlin/com/openmeteo/api/common/time/Time.kt
@@ -3,4 +3,6 @@ package com.openmeteo.api.common.time
 import kotlinx.serialization.Serializable
 
 @Serializable(with = TimeSerializer::class)
-class Time(seconds: Long) : java.util.Date(seconds * 1000)
+class Time(seconds: Long) : java.util.Date(seconds * 1000) {
+    constructor(seconds: Double) : this(seconds.toLong())
+}
