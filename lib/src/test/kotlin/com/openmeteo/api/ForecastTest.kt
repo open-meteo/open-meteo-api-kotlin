200d69a15ca64fda9135abc934911efcaf823cf4
DadiBit <38182988+DadiBit@users.noreply.github.com>
2023-06-04 18:15:50 +0200

    feat: `Forecast` endpoint

---
+++ lib/src/test/kotlin/com/openmeteo/api/ForecastTest.kt
@@ -0,0 +1,54 @@
+package com.openmeteo.api
+
+import com.openmeteo.api.common.Options
+import com.openmeteo.api.common.http.Endpoint
+import com.openmeteo.api.common.query.City
+import com.openmeteo.api.common.time.Timezone
+import com.openmeteo.api.common.units.Unit
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+class ForecastTest {
+
+    companion object {
+        val forecast = Endpoint(Forecast.context)
+    }
+
+    @Test
+    fun `Amsterdam, with past day`() {
+        val query = Forecast.Query(
+            latitude = City.Amsterdam.latitude,
+            longitude = City.Amsterdam.longitude,
+            daily = Options.list(Forecast.Daily) { of(
+                weathercode, sunrise, sunset, temperature2mMax, temperature2mMin
+            ) },
+            timezone = Timezone.auto,
+            pastDays = 1,
+        )
+        forecast.query<Forecast.Response, Forecast.Query>(query)
+            .getOrThrow().run {
+                assertEquals(52.38f, latitude)
+                assertEquals(4.9f, longitude)
+                // assertEquals(utcOffsetSeconds, 7200) // TODO: changes based on summer time?
+                assertEquals("Europe/Amsterdam", timezone.id)
+                assertEquals("CEST", timezoneAbbreviation)
+                assertEquals(17f, elevation)
+                assert(dailyUnits.equals(mapOf(
+                    "time" to Unit.UnixTime,
+                    "weathercode" to Unit.WeatherCode,
+                    "sunrise" to Unit.UnixTime,
+                    "sunset" to Unit.UnixTime,
+                    "temperature_2m_max" to Unit.Celsius,
+                    "temperature_2m_min" to Unit.Celsius,
+                )))
+                assertContains(dailyValues, "time")
+                assertContains(dailyValues, "weathercode")
+                assertContains(dailyValues, "sunrise")
+                assertContains(dailyValues, "sunset")
+                assertContains(dailyValues, "temperature_2m_max")
+                assertContains(dailyValues, "temperature_2m_min")
+            }
+    }
+
+}
