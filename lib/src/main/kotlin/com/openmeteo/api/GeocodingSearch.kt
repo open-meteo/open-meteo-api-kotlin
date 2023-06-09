9b02b4e560e5591c9b110942b394a5d850833404
DadiBit <38182988+DadiBit@users.noreply.github.com>
2023-06-09 17:49:39 +0200

    feat: `GeocodingSearch` api

---
+++ lib/src/main/kotlin/com/openmeteo/api/GeocodingSearch.kt
@@ -0,0 +1,35 @@
+package com.openmeteo.api
+
+import com.openmeteo.api.common.http.ContentFormat
+import com.openmeteo.api.common.http.Endpoint
+import kotlinx.serialization.Serializable
+import java.net.URL
+import com.openmeteo.api.common.Response as R
+import com.openmeteo.api.common.query.Query as Q
+
+object GeocodingSearch : Endpoint(
+    URL("https://geocoding-api.open-meteo.com/v1/search")
+) {
+
+    operator fun invoke(query: Query) = query<Response, Query>(query)
+
+    @Serializable
+    open class Query(
+        val name: String,
+        val count: Int? = null,
+        val language: String? = null,
+        override val apikey: String?,
+        override val format: ContentFormat? = ContentFormat.ProtoBuf,
+    ) : Q.ContentFormat, Q.CommercialLicense {
+        init {
+            require(name.length > 1)
+        }
+    }
+
+    @Serializable
+    open class Response(
+        val results: Array<GeocodingGet.Response>,
+        override val generationtimeMs: Float,
+    ) : R.GenerationTimed
+
+}
