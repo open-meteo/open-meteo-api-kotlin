bb170d35da42879bec6a41939c7425f76df801e4
DadiBit <38182988+DadiBit@users.noreply.github.com>
2023-07-01 17:54:08 +0200

    chore: reflect new folder structure in version.sh

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -6,12 +6,12 @@
 bump() {
   [ -v "version" ] || exit 1
 
-  sed -i "s/version = '.*'/version = '$version'/" "lib/build.gradle"
+  sed -i "s/version = '.*'/version = '$version'/" "build.gradle"
   sed -i "s/implementation 'com.open-meteo:open-meteo-api-kotlin:.*'/implementation 'com.open-meteo:open-meteo-api-kotlin:$version'/" "INSTALLATION.md"
   sed -i "s/<version>.*<\/version>/<version>$version<\/version>/" "INSTALLATION.md"
 
-  git add "lib/build.gradle" "INSTALLATION.md"
-  git commit -m "Version $version" -- "lib/build.gradle" "INSTALLATION.md"
+  git add "build.gradle" "INSTALLATION.md"
+  git commit -m "Version $version" -- "build.gradle" "INSTALLATION.md"
   git tag "$version"
   # git push origin "$version" --tags
 }
