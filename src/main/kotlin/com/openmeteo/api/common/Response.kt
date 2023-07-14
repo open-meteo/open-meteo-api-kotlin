969b1f1c7628e8785ffece55d48a7f10b9e237f7
DadiBit <38182988+DadiBit@users.noreply.github.com>
2023-07-14 11:36:03 +0200

    feat: "gluable" unit and values
    You may now do:
    ```
    forecast.daily[temperature2mMax] // get the daily variable "temperature_2m_max"
    ?.run { // run a scoped block in the `UnitTimeStepValues` object
        // print the header with the variable string and the unit string (ie: Celsius, not °C)
        println("# $temperature2mMax ($unit)")
        // print a line for each time step (ie day), with the time and the value for that time step
        values.forEach{ (t, v) -> println("> $t | $v") }
    }
    ```

--- src/main/kotlin/com/openmeteo/api/common/Response.kt
+++ src/main/kotlin/com/openmeteo/api/common/Response.kt
@@ -1,5 +1,6 @@
 package com.openmeteo.api.common
 
+import com.openmeteo.api.common.time.Time
 import com.openmeteo.api.common.units.Units
 
 interface Response {
@@ -25,12 +26,51 @@ interface Response {
         val generationtimeMs: Float
     }
 
+    class UnitTimeStepValues(
+        val unit: Units,
+        val values: Map<Time, Double?>
+    ) {
+        @RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.")
+        @Retention(AnnotationRetention.BINARY)
+        @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY)
+        annotation class GlueUnitTimeStepValues // Opt-in requirement annotation
+
+        companion object {
+
+            /**
+             * Glue units and values together into a map of keys (eg: temperature_2m) and unit/values
+             */
+            @GlueUnitTimeStepValues
+            internal fun glue(units:  Map<String, Units>, values: Map<String, Array<Double?>>) : Map<String, UnitTimeStepValues> {
+                if (values.size != units.size)
+                    throw Error("Malformed data: units and values size do not match! We either didn't get enough units or enough values!")
+                    // TODO: add option to magically fix shit on its own.
+                if (values.isEmpty())
+                    return emptyMap()
+                // get just the time keys array
+                val time = values.getValue("time")
+                    .requireNoNulls()
+                    // get Time objects
+                    .map { Time(it) }
+                // filter time away! We don't need it in the map values
+                return values.filterKeys { it != "time" }
+                    .mapValues { (k, v) -> UnitTimeStepValues(
+                        units.getValue(k), // make sure that each keys exists in the units value
+                        time.zip(v).toMap()) // create a map with the time as keys and the values as... values :)
+                    }
+            }
+        }
+    }
+
     /**
      * Resources that may have daily data entries.
      */
     interface Daily : Response {
         val dailyUnits: Map<String, Units>
         val dailyValues: Map<String, Array<Double?>>
+        @UnitTimeStepValues.GlueUnitTimeStepValues
+        val daily get() = UnitTimeStepValues
+            .glue(dailyUnits, dailyValues)
     }
 
     /**
@@ -39,6 +79,9 @@ interface Response {
     interface Hourly : Response {
         val hourlyUnits: Map<String, Units>
         val hourlyValues: Map<String, Array<Double?>>
+        @UnitTimeStepValues.GlueUnitTimeStepValues
+        val hourly get() = UnitTimeStepValues
+            .glue(hourlyUnits, hourlyValues)
     }
 
     /**
