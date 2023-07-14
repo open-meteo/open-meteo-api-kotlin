0e5d4b85aa5c925f128aaf63ed72e647d0b434a5
DadiBit <38182988+DadiBit@users.noreply.github.com>
2023-07-14 11:30:49 +0200

    feat: time to string is formatted as `MM-dd hh:mm`

--- src/main/kotlin/com/openmeteo/api/common/time/Time.kt
+++ src/main/kotlin/com/openmeteo/api/common/time/Time.kt
@@ -1,8 +1,16 @@
 package com.openmeteo.api.common.time
 
 import kotlinx.serialization.Serializable
+import java.text.SimpleDateFormat
 
 @Serializable(with = TimeSerializer::class)
 class Time(seconds: Long) : java.util.Date(seconds * 1000) {
+
+    companion object Format : SimpleDateFormat("MM-dd hh:mm")
+
     constructor(seconds: Double) : this(seconds.toLong())
+
+    override fun toString(): String =
+        format(this)
+
 }
