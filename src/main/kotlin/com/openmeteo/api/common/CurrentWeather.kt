b8b41f5903b8fa1b44ce776bce93f370c77cf7bc
DadiBit <38182988+DadiBit@users.noreply.github.com>
2023-07-15 21:55:18 +0200

    doc: current weather properties documentation

--- src/main/kotlin/com/openmeteo/api/common/CurrentWeather.kt
+++ src/main/kotlin/com/openmeteo/api/common/CurrentWeather.kt
@@ -5,35 +5,36 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
- *
+ * The current weather status.
  */
 @Serializable
 open class CurrentWeather(
     /**
-     *
+     * The "current" time step to which the data is referred to.
+     * It usually differs from the Date/Time of "now".
      */
     val time: Time,
     /**
-     *
+     * The temperature.
      */
     val temperature: Float,
     /**
-     *
+     * The average wind speed.
      */
     @SerialName("windspeed")
     val windSpeed: Float,
     /**
-     *
+     * The wind direction.
      */
     @SerialName("winddirection")
     val windDirection: Float,
     /**
-     *
+     * The WMO [WeatherCode].
      */
     @SerialName("weathercode")
     val weatherCode: WeatherCode,
     /**
-     * Whether the time step has daylight
+     * Whether the time step has daylight or not.
      */
     @Serializable(with = IntAsBoolean::class)
     val isDay: Boolean,
