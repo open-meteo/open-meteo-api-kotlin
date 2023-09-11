7d2d27b81822806888ecd9101a34f66ad1ce07c6
DadiBit <38182988+DadiBit@users.noreply.github.com>
2023-09-11 21:34:44 +0200

    docs: `OpenMeteo` methods in-code documentation

--- src/main/kotlin/com/openmeteo/api/OpenMeteo.kt
+++ src/main/kotlin/com/openmeteo/api/OpenMeteo.kt
@@ -27,6 +27,10 @@ import java.net.URL
  *     }
  * }
  * ```
+ *
+ * @param latitude The stored location latitude.
+ * @param longitude The stored location longitude.
+ * @param apikey The optional API key.
  */
 open class OpenMeteo(
     var latitude: Float,
@@ -55,14 +59,21 @@ open class OpenMeteo(
                 this.apikey = apikey
             }.map { OpenMeteo(it.latitude, it.longitude, apikey) }
 
+        /**
+         * Retrieve an [OpenMeteo] instance of a specific location.
+         * @param id The location unique id.
+         * @param apikey The optional API key.
+         */
         operator fun invoke(id: Int, apikey: String? = null) =
             GeocodingGet(id) { this.apikey = apikey }.map { OpenMeteo(it.latitude, it.longitude, apikey) }
 
         /**
-         * Search a location with the Geocoding API.
-         * Fetches one element and uses its coordinates by default.
-         * Note that language is only used in the query and it is **not** stored
-         * (e.g. search "Roma" instead of "Rome", with "it" as language).
+         * Search a location with the Geocoding API. Fetches one element and uses its coordinates by default.
+         * For more flexibility use the full [GeocodingSearch] API.
+         * @param location The name of the location to search for.
+         * @param language The language to use for the search (e.g. search "Roma" instead of "Rome" with "it" as
+         * language). Note that language is only used in the query and is **not stored**.
+         * @param apikey The optional API key.
          */
         operator fun invoke(location: String, language: String? = null, apikey: String? = null) =
             GeocodingSearch(location) {
@@ -76,11 +87,24 @@ open class OpenMeteo(
 
     }
 
+    /**
+     * Query the [AirQuality] API.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun airQuality(
         context: URL = AirQuality.context,
         query: AirQuality.Query.() -> Unit,
     ) = AirQuality(latitude, longitude, apikey, context, query)
 
+    /**
+     * Query the [ClimateChange] API.
+     * @param models The [ClimateChange.Models] to use.
+     * @param startDate The time frame start date.
+     * @param endDate The time frame end date.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun climateChange(
         models: String,
         startDate: Date,
@@ -89,26 +113,53 @@ open class OpenMeteo(
         query: ClimateChange.Query.() -> Unit,
     ) = ClimateChange(latitude, longitude, models, startDate, endDate, apikey, context, query)
 
+    /**
+     * Query the [Elevation] API.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun elevation(
         context: URL = Elevation.context,
         query: Elevation.Query.() -> Unit,
     ) = Elevation(latitude, longitude, apikey, context, query)
 
+    /**
+     * Query the [Ensemble] API.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun ensemble(
         context: URL = Ensemble.context,
         query: Ensemble.Query.() -> Unit,
     ) = Ensemble(latitude, longitude, apikey, context, query)
 
+    /**
+     * Query the [Flood] API.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun flood(
         context: URL = Flood.context,
         query: Flood.Query.() -> Unit,
     ) = Flood(latitude, longitude, apikey, context, query)
 
+    /**
+     * Query the [Forecast] API.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun forecast(
         context: URL = Forecast.context,
         query: Forecast.Query.() -> Unit,
     ) = Forecast(latitude, longitude, apikey, context, query)
 
+    /**
+     * Query the [Historical] API.
+     * @param startDate The time frame start date.
+     * @param endDate The time frame end date.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun historical(
         startDate: Date,
         endDate: Date,
@@ -116,6 +167,11 @@ open class OpenMeteo(
         query: Historical.Query.() -> Unit,
     ) = Historical(latitude, longitude, startDate, endDate, apikey, context, query)
 
+    /**
+     * Query the [Marine] API.
+     * @param context The API endpoint to use. Useful for self-hosted server instances.
+     * @param query The query modifier.
+     */
     inline fun marine(
         context: URL = Marine.context,
         query: Marine.Query.() -> Unit,
